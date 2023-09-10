177c07a71ab47e5896b24b03c09f64aecb0f0212
leul mekonnen <leulmekonnen049@gmail.com>
2023-09-10 03:58:26 -0700

    new clothing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
 import Homepage from './components/page/homepage/homepage.component';
+import  { Routes, Route } from 'react-router-dom';
 
+function Hatspage() {
+  return (
+    <div>
+      <h1>HATS PAGE </h1>
+    </div>
+  )
+}
 function App() {
   return (
-  <div>
+<div>
+  <Routes>
+      <Route exact path='/' Component={Homepage} />
+      <Route path='/hat' Component={Hatspage} />
+  </Routes>
+    
+
+    
    
-    <Homepage />
    </div>
   );
 }
